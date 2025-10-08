b943e9cbe1782cf3180eba4ab892b8552c0480be
Agnieszka Koń-Kogut <konagnieszka@gmail.com>
2025-10-08 16:09:12 +0200

    feat: layout adjustments

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,18 +1,14 @@
-import React from 'react'
-import '@/app/globals.css'
-import Header from '@/components/Header'
+import React from "react";
+import "@/app/globals.css";
+import Header from "@/components/Header";
 
-export default function RootLayout({
-                                            children,
-                                        }: {
-    children: React.ReactNode
-}) {
-    return (
-        <html lang="en">
-        <body className="bg-gradient px-20 py-16">
-        <Header/>
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang="en" className="font-sans text-primary">
+      <body className="bg-gradient px-48 py-16">
+        <Header />
         <main>{children}</main>
-        </body>
-        </html>
-    )
+      </body>
+    </html>
+  );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,7 @@
+import HeroSection from "@/components/HeroSection";
+
 export default function Home() {
   return (
-      <div>
-          <h2>cześć, jestem</h2>
-        <h1 className="text-2xl font-bold">Agnieszka
-            Koń-Kogut</h1>
-        <p>frontend developer</p>
-      </div>
+     <HeroSection/>
   );
 }
