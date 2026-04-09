76225234d588eda020e45e5b20d1b7be11930703
Agnieszka Koń-Kogut <konagnieszka@gmail.com>
2026-04-09 15:20:33 +0200

    test: Button update

--- src/components/ui/Button.test.tsx
+++ src/components/ui/Button.test.tsx
@@ -1,4 +1,5 @@
 import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 
 import Button from './Button';
 
@@ -6,5 +7,13 @@ describe('Button', () => {
   it('should render with text', () => {
     render(<Button text="test" />);
     expect(screen.getByText('test')).toBeInTheDocument();
+  });
+  it('should handle click', async () => {
+    const handleClick = jest.fn();
+    render(<Button text="test" onClick={handleClick} />);
+    expect(screen.getByText('test')).toBeInTheDocument();
+    const element = screen.getByText('test');
+    await userEvent.click(element);
+    expect(handleClick).toHaveBeenCalledTimes(1);
   });
 });
