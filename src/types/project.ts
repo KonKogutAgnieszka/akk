d57f1750984f94ed52a93c8e00723fa39d3208a4
Agnieszka Koń-Kogut <konagnieszka@gmail.com>
2026-06-13 21:38:59 +0200

    refactor: make image optional in project type

--- src/types/project.ts
+++ src/types/project.ts
@@ -3,7 +3,7 @@ import { TechnologyKey } from '@/data/technologies';
 export type Project = {
   id: number;
   title: string;
-  image: string;
+  image?: string;
   description: string;
   stack: TechnologyKey[];
   liveUrl?: string;
